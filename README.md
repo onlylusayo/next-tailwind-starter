f5ba5a0235c0ee297b7be5fbf7fe8dd1fa18a2fe
OnlyLusayo <onlylusayo@gmail.com>
2023-02-20 10:27:32 +0200

    Config: Tailwind, Custom App Starter

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# next-tailwind-starter
-Starter files for next project with tailwind
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Welcome to the Tailwind Project Starter. 
+  </h1>
+  )
+}
